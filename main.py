2f6b7abd3d6459cdab91d8bb44b9773e80e26087
ulkshifer <kamza.0508@gmail.com>
2022-10-12 10:09:00 +0600

    fourth commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Iterating through a string
 
-n="i am laxmi"
-s=n.split(" ")
-
+string = "geeks quiz practice code"
+s = string.split()[::-1]
+l = []
 for i in s:
 
-  if len(i)%2==0:
-    print(i)
+    l.append(i)
+
+print(" ".join(l))
