a899cc2a8c1f876ee63d2aca55ce8bf83355a032
ulkshifer <kamza.0508@gmail.com>
2022-10-12 14:05:19 +0600

    7 commit

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-def append_middle(s1, s2):
-    print("Original Strings are", s1, s2)
+str1 = "PYnAtivE"
+print('Original String:', str1)
+lower = []
+upper = []
+for char in str1:
+    if char.islower():
+        lower.append(char)
+    else:
+        upper.append(char)
 
-    # middle index number of s1
-    mi = int(len(s1) / 2)
-
-    # get character from 0 to the middle index number from s1
-    x = s1[:mi:]
-    # concatenate s2 to it
-    x = x + s2
-    # append remaining character from s1
-    x = x + s1[mi:]
-    print("After appending new string in middle:", x)
-
-append_middle("Ault", "Kelly")
+sorted_str = ''.join(lower + upper)
+print('Result:', sorted_str)
